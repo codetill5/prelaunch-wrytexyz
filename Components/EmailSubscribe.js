c606a05c6f62c5ad690a7ad7dcb836fb590058f2
codetill5 <mehrashubham216@gmail.com>
2022-08-29 23:56:36 +0530

    added: mailchim integration with ui

---
+++ Components/EmailSubscribe.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Modal from "react-modal";
+import { isMobile } from "react-device-detect";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "30px",
+    overflow: "visible",
+    width: `${isMobile ? "80%" : ""}`,
+  },
+};
+
+Modal.defaultStyles.overlay.backgroundColor = "rgba(0, 0, 0, 0.65)";
+
+const EmailSubscribe = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const postUrl = `${process.env.NEXT_PUBLIC_BASEURL_MAILCHIMP}?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U}&id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID}`;
+
+  const CustomForm = ({ status, message, onValidated }) => {
+    const [email, setEmail] = useState("");
+
+    const handleChange = (e) => {
+      setEmail(e.target.value);
+    };
+
+    const handleSubmitForm = (e) => {
+      e.preventDefault();
+      email &&
+        email.indexOf("@") > -1 &&
+        onValidated({
+          EMAIL: email,
+        });
+    };
+
+    const openModal = () => {
+      setIsOpen(true);
+    }
+
+    const closeModal = () => {
+      setIsOpen(false);
+    }
+
+    return (
+      <div>
+        <div className="btnContainer">
+          <button onClick={openModal} className="earlyAccessBtn">
+            Get Early Access
+          </button>
+        </div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <span className="close" onClick={() => closeModal()}>
+            <p>x</p>
+          </span>
+          <form
+            className="mc__form"
+            onSubmit={(e) => handleSubmitForm(e)}
+            onValidated={(formData) => subscribe(formData)}
+          >
+            <div className="formInnerContainer">
+              <label className="emailLabel">EMAIL ADDRESS</label>
+              <input
+                type="text"
+                placeholder="name@email.com"
+                className="emailInput"
+                onChange={(e) => handleChange(e)}
+                value={email}
+                required
+              />
+              <button className="submitBtn" formValues={[email]}>
+                Get Early Access
+              </button>
+            </div>
+          </form>
+        </Modal>
+      </div>
+    );
+  };
+
+  return (
+    <MailchimpSubscribe
+      url={postUrl}
+      render={({ subscribe, status, message }) => (
+        <CustomForm
+          status={status}
+          message={message}
+          onValidated={(formData) => subscribe(formData)}
+        />
+      )}
+    />
+  );
+};
+
+export default EmailSubscribe;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import EmailSubscribe from "../Components/EmailSubscribe";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -40,6 +41,7 @@ export default function Home() {
           />
         </div>
       </a>
+      <EmailSubscribe />
     </div>
   );
 }
